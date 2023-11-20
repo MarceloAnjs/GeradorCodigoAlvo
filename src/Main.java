59af2b62c85f3655a531face090095273c163e24
Marcelo Anjos <marceloanjos023@gmail.com>
2023-11-20 09:20:00 -0300

    Add Parser class and implement basic functionality

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import parser.Parser;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Parser p = new Parser("src/projects/07/MemoryAccess/BasicTest/BasicTest.vm");
+        while (p.hasMoreCommands()) {
+            String[] command = p.command();
+            for (String token : command) {
+                System.out.print(token + " ");
+            }
+            System.out.println();
+
+        }
     }
 }
