c164cb8f9fc8258c7cb74c683b093eaa47491467
ce.ferreira <ferreiraeduardo822@gmail.com>
2023-12-01 23:07:57 -0300

    Parser updates

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -1,35 +1,37 @@
 package parser;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Parser {
-    private BufferedReader reader;
-    private List<String[]> tokens;
+    
+    private Scanner scanner;
+    private String[] currentCommand;
+    private final String C_PUSH = "C_PUSH";
+    private final String C_POP = "C_POP";
+    private final String C_ARITHMETIC = "C_ARITHMETIC";
+    private final String C_GOTO = "C_GOTO";
+    private final String C_LABEL = "C_LABEL";
+    private final String C_IF = "C_IF";
+    private final String C_RETURN = "C_RETURN";
+    private final String C_FUNCTION = "C_FUNCTION";
+    private final String C_CALL = "C_CALL";
 
-    public Parser(String filename) {
+    public Parser(String filePath) {
+        File fileToParse = new File(filePath);
         try {
-            reader = new BufferedReader(new FileReader(filename));
-            tokens = new ArrayList<>();
-            String line;
-            while ((line = reader.readLine()) != null) {
-                if (!line.contains("//") && !line.trim().equals("")) {
-                    tokens.add(line.split("\\s+"));
-                }
-            }
-        } catch (IOException e) {
+            this.scanner = new Scanner(fileToParse);
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
     }
 
-    public String[] command() {
-        return tokens.remove(0);
-    }
-
     public boolean hasMoreCommands() {
-        return !tokens.isEmpty();
+        if (scanner.hasNextLine()) {
+            return true;
+        }
+        scanner.close();
+        return false;
     }
 }
